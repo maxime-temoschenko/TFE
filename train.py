3bc1ab7654304553fdaa35048070652cf9a3e619
mtemoschenko <maxime.temoschenko@student.uliege.be>
2025-02-07 10:37:47 +0100

    🏋️ Training on 64x64

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import h5py
 import math
 import torch
+import numpy as np
 #import wandb
 import torch.nn as nn
 import torch.optim as optim
@@ -9,129 +10,115 @@ from torch.utils.data import DataLoader
 from tqdm import trange
 from pathlib import Path
 from utils import TrajectoryDataset
-from score import ScoreUNet, MCScoreWrapper
-from score import VPSDE, MCScoreNet
+from score import ScoreUNet, MCScoreWrapper, VPSDE
+from score import VPSDE
 
 PATH = Path('.')
 
 with h5py.File(PATH / "data/mask.h5", "r") as f:
-    mask = torch.tensor(f["dataset"][:], dtype=torch.float32).unsqueeze(0)  # Shape
+    mask = torch.tensor(f["dataset"][:], dtype=torch.float32).unsqueeze(0)
 
-def masked_vpsde_loss(sde, x, mask):
-    w = mask.expand_as(x)
-    return sde.loss(x, w=w)
+if torch.isnan(mask).any():
+    raise ValueError("Mask contains NaN values!")
 
-CONFIG = {
-    # Architecture
-    'window': 5,
-    'embedding': 64,
-    'hidden_channels': (96, 192, 384),
-    'hidden_blocks': (3, 3, 3),
-    'kernel_size': 3,
-    'activation': 'SiLU',
-    # Training
-    'epochs': 4096,
-    'batch_size': 5,
-    'optimizer': 'AdamW',
-    'learning_rate': 2e-4,
-    'weight_decay': 1e-3,
-    'scheduler': 'linear',
+TRAIN_CONFIG = {
+    "epochs": 100,
+    "batch_size": 5,
+    "learning_rate": 1e-3,
+    "weight_decay": 1e-3,
+    "scheduler": "linear",
+    "embedding": 32,
+    "hidden_channels": (64,),
+    "hidden_blocks": (3,),
+    "activation": "SiLU",
 }
+device = 'cpu'
 
-trainset = TrajectoryDataset(PATH / "data/train.h5", window=10)
-validset = TrajectoryDataset(PATH / "data/valid.h5", window=10)
-
-trainloader = DataLoader(trainset, batch_size=CONFIG["batch_size"], shuffle=True, num_workers=1, persistent_workers=True)
-validloader = DataLoader(validset, batch_size=CONFIG["batch_size"], shuffle=False, num_workers=1, persistent_workers=True)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-#Create the model :
-window = CONFIG['window']
-score = MCScoreNet(2, order=window // 2)
-score.kernel = ScoreUNet(
-    channels=window * 2,
-    embedding=CONFIG['embedding'],
-    hidden_channels=CONFIG['hidden_channels'],
-    hidden_blocks=CONFIG['hidden_blocks'],
-    kernel_size=CONFIG['kernel_size'],
-    activation=nn.SiLU,
-    spatial=2,
-    padding_mode='circular',
-)
 '''
-score_model =  MCScoreWrapper(ScoreUNet(
-        channels=4,
-        embedding=CONFIG["embedding"],
-        hidden_channels=CONFIG["hidden_channels"],
-        hidden_blocks=CONFIG["hidden_blocks"],
-        activation=nn.SiLU,
-    )).to(device)
+Definition of Denoiser and Scheduler
 '''
-sde = VPSDE(score, shape=(window*2, 55, 66)).to(device)
+# Common dimensions
+batch_size = 5
+MAR_channels = 4 # Example ['RF', 'T2m', 'U10m', MASK]
+window = 10
+y_dim = 128
+x_dim = 128
 
-optimizer = optim.AdamW(sde.parameters(), lr=CONFIG["learning_rate"], weight_decay=CONFIG["weight_decay"])
+MODEL_CONFIG = { 'hidden_channels' : [32, 64, 128],  \
+'hidden_blocks' : [2, 3, 5],  \
+'spatial' : 2, \
+'channels' : window*MAR_channels, \
+'context' : 0,\
+'embedding' : 64 }
+
+# Denoiser and Scheduler
+score_unet = ScoreUNet(**MODEL_CONFIG)
+vpsde = VPSDE(score_unet, shape=(MAR_channels*window, y_dim, x_dim))
+
+# Load Dataset
+trainset = TrajectoryDataset(PATH / "data/train.h5", window=10, flatten=True)
+validset = TrajectoryDataset(PATH / "data/valid.h5", window=10, flatten=True)
+
+trainloader = DataLoader(trainset, batch_size=TRAIN_CONFIG["batch_size"], shuffle=True, num_workers=1, persistent_workers=True)
+validloader = DataLoader(validset, batch_size=TRAIN_CONFIG["batch_size"], shuffle=False, num_workers=1, persistent_workers=True)
+
+
+optimizer = optim.AdamW(vpsde.parameters(), lr=TRAIN_CONFIG["learning_rate"], weight_decay=TRAIN_CONFIG["weight_decay"])
 
 # Define Learning Rate Scheduler
-if CONFIG["scheduler"] == "linear":
-    lr_lambda = lambda t: 1 - (t / CONFIG["epochs"])
-elif CONFIG["scheduler"] == "cosine":
-    lr_lambda = lambda t: (1 + math.cos(math.pi * t / CONFIG["epochs"])) / 2
-elif CONFIG["scheduler"] == "exponential":
-    lr_lambda = lambda t: math.exp(-7 * (t / CONFIG["epochs"]) ** 2)
+if TRAIN_CONFIG["scheduler"] == "linear":
+    lr_lambda = lambda t: 1 - (t / TRAIN_CONFIG["epochs"])
+elif TRAIN_CONFIG["scheduler"] == "cosine":
+    lr_lambda = lambda t: (1 + math.cos(math.pi * t / TRAIN_CONFIG["epochs"])) / 2
+elif TRAIN_CONFIG["scheduler"] == "exponential":
+    lr_lambda = lambda t: math.exp(-7 * (t / TRAIN_CONFIG["epochs"]) ** 2)
 else:
     raise ValueError("Invalid scheduler type")
 
 scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
 
-for epoch in (bar := trange(CONFIG["epochs"], ncols=88)):
+all_losses_train = []
+all_losses_valid = []
+for epoch in (bar := trange(TRAIN_CONFIG["epochs"], ncols=88)):
     losses_train = []
     losses_valid = []
 
-    ## Train
-    sde.train()
-    print('-------')
-    print('[TRAIN LOOP]')
+    # Training
+    vpsde.train()
     for i, (batch, _) in enumerate(trainloader):
-        print(f"{i}")
+        if torch.isnan(batch).any():
+            raise ValueError("batch contains NaN values!")
         batch = batch.to(device)
         optimizer.zero_grad()
 
-        # **Apply Mask**
-        mask_batch = mask.to(device).expand_as(batch)  # Expand mask to match batch size
-        w = mask_batch.float()  # Convert mask to weight format
+        # Mask
+        mask_batch = mask.to(device).expand_as(batch)
+        w = mask_batch.float()
 
-        # **Compute VPSDE Loss**
-        loss = sde.loss(batch, w=w)
+
+        loss = vpsde.loss(batch, w=w)
         loss.backward()
         optimizer.step()
 
         losses_train.append(loss.detach())
-    print('[\TRAIN LOOP]')
-    print('-------')
-    ## Validation
-    sde.eval()
+
+    # Evaluation
+    vpsde.eval()
     with torch.no_grad():
-        print('-------')
-        print('[VALID LOOP]')
         for batch, _ in validloader:
             batch = batch.to(device)
             mask_batch = mask.to(device).expand_as(batch)
             w = mask_batch.float()
 
-            loss = sde.loss(batch, w=w)
+            loss = vpsde.loss(batch, w=w)
             losses_valid.append(loss)
-        print('-------')
-        print('[VALID LOOP]')
-    ## Compute Loss Stats
+
     loss_train = torch.stack(losses_train).mean().item()
     loss_valid = torch.stack(losses_valid).mean().item()
+    print(f"Train Loss : {loss_train}, Valid Loss : {loss_valid}")
     lr = optimizer.param_groups[0]['lr']
 
-    ## Step Scheduler
-    scheduler.step()
+    all_losses_train.append(loss_train)
+    all_losses_valid.append(loss_valid)
 
-    # Save model periodically
-    if (epoch + 1) % 10 == 0:
-        torch.save(sde.state_dict(), PATH / f"checkpoints/model_epoch{epoch+1}.pth")
+    scheduler.step()
